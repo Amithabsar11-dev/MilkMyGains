26e9607f4753a3169a30ea339815e15f2407b8ec
Avacado <avacado@Avacado.local>
2024-12-25 13:45:11 +0530

    latest-push

--- mmg-app/src/preload.js
+++ mmg-app/src/preload.js
@@ -1,39 +1,54 @@
 import React, { useEffect, useState } from 'react';
 import './preload.css';
+import Background from './assets/background.png';
 
 const Preload = () => {
   const [rippleAnimated, setRippleAnimated] = useState(false);
+  const [showWhiteText, setShowWhiteText] = useState(false);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
+    const rippleTimer = setTimeout(() => {
       setRippleAnimated(true);
-    }, 3500); 
+    }, 2200); 
 
-    return () => clearTimeout(timer);
+    const textTimer = setTimeout(() => {
+      setShowWhiteText(true);
+    }, 2500); 
+
+    return () => {
+      clearTimeout(rippleTimer);
+      clearTimeout(textTimer);
+    };
   }, []);
 
   return (
     <div className="preload-container">
-      <div className="word">
-        {['P', 'U', 'R', 'E'].map((letter, index) => (
-          <span key={index} className="letter" style={{ animationDelay: `${index * 0.2}s` }}>
-            {letter}
-          </span>
-        ))}
-      </div>
-      <div className="word">
-        {['P', 'O', 'W', 'E', 'R', 'F', 'U', 'L'].map((letter, index) => (
-          <span key={index} className="letter" style={{ animationDelay: `${(index + 4) * 0.2}s` }}>
-            {letter}
-          </span>
-        ))}
-      </div>
-      <div className="word">
-        {['P', 'R', 'O', 'T', 'E', 'I', 'N'].map((letter, index) => (
-          <span key={index} className="letter" style={{ animationDelay: `${(index + 12) * 0.2}s` }}>
-            {letter}
-          </span>
-        ))}
+      {/* Image overlay */}
+      <div className="image-overlay"></div>
+
+      {/* Text Content */}
+      <div className={`word-alignment ${showWhiteText ? 'text-white' : ''}`}>
+        <div className="word">
+          {['P', 'U', 'R', 'E'].map((letter, index) => (
+            <span key={index} className="letter" style={{ animationDelay: `${index * 0.1}s` }}>
+              {letter}
+            </span>
+          ))}
+        </div>
+        <div className="word">
+          {['P', 'O', 'W', 'E', 'R', 'F', 'U', 'L'].map((letter, index) => (
+            <span key={index} className="letter" style={{ animationDelay: `${(index + 4) * 0.1}s` }}>
+              {letter}
+            </span>
+          ))}
+        </div>
+        <div className="word">
+          {['P', 'R', 'O', 'T', 'E', 'I', 'N'].map((letter, index) => (
+            <span key={index} className="letter" style={{ animationDelay: `${(index + 12) * 0.1}s` }}>
+              {letter}
+            </span>
+          ))}
+        </div>
       </div>
 
       {/* Ripple Element */}
---
+++ mmg-app/src/sample.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function sample() {
+  return (
+    <div className='container' style={{height:"400vh"}}>
+        <h1 style={{position:"sticky", top:0}}> Hiii</h1>
+    </div>
+  )
+}
